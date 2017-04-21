e86f605d76125fa27869ab5d4fd1a31adca8a10c
MihaMasic <mmasic@tvz.hr>
2017-04-21 18:49:42 +0200

    Filtriranje iznajmljenih ordera

--- src/com/pios/web/controller/Details.java
+++ src/com/pios/web/controller/Details.java
@@ -1,6 +1,7 @@
 package com.pios.web.controller;
 
 import com.pios.persistence.model.Accommodation;
+import com.pios.persistence.model.Order;
 import com.pios.service.AccommodationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -9,6 +10,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Created by Matija on 14.4.2017..
@@ -25,6 +31,9 @@ public class Details {
     @GetMapping("/{accommodationId}")
     public String openDetails(Model model, @PathVariable int accommodationId){
         Accommodation accommodation = accommodationService.getAccommodation(accommodationId);
+        Set<Order> orders = accommodation.getOrders().stream().filter(p -> !p.getReserved()).collect(Collectors.toSet());
+        accommodation.setOrders(orders);
+
         model.addAttribute("accommodation", accommodation);
         return "details";
     }
